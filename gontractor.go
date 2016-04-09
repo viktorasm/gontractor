3cdebf5ce854b94aa8585ba9e7bb67d506a453aa
Viktoras Makauskas <viktoras@neglostyti.com>
2016-04-09 18:17:40 +0300

    go fmt

--- gontractor.go
+++ gontractor.go
@@ -1,32 +1,31 @@
 package main
+
 import (
 	"flag"
-	"github.com/viktorasm/gontractor/swagger"
 	"github.com/viktorasm/gontractor/generate"
+	"github.com/viktorasm/gontractor/swagger"
 )
 
-
-
 type Gontractor struct {
-	spec string
+	spec           string
 	serverTemplate string
-	serverOutFile string
-	clientOutFile string
-	apiOutFile string
-	outDir string
+	serverOutFile  string
+	clientOutFile  string
+	apiOutFile     string
+	outDir         string
 }
 
 func NewGontractor() *Gontractor {
-	return &Gontractor {
-		spec:"swagger.yaml",
+	return &Gontractor{
+		spec:          "swagger.yaml",
 		serverOutFile: "server_generated.go",
 		clientOutFile: "client/client.go",
-		apiOutFile: "api/api.go",
-		outDir: ".",
+		apiOutFile:    "api/api.go",
+		outDir:        ".",
 	}
 }
 
-func (g Gontractor) saveFile(fileName string,contents string) error {
+func (g Gontractor) saveFile(fileName string, contents string) error {
 	return nil
 }
 
@@ -36,17 +35,14 @@ func (g Gontractor) Execute() error {
 	generator.SetTagGenerators(generate.JsonTags)
 
 	apiContents := generator.GenerateApiInterface(*spec)
-	g.saveFile(g.apiOutFile,apiContents)
+	g.saveFile(g.apiOutFile, apiContents)
 	return nil
 }
-
 
 func main() {
 	g := NewGontractor()
 
-	flag.StringVar(&g.spec,"spec","swagger.yaml","service specification flag")
-	flag.StringVar(&g.serverTemplate, "server-template","","template to generate server")
-
-
+	flag.StringVar(&g.spec, "spec", "swagger.yaml", "service specification flag")
+	flag.StringVar(&g.serverTemplate, "server-template", "", "template to generate server")
 
 }
