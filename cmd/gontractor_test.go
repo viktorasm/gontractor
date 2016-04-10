16b392210b987137b371d5aaa59f639b236faa32
Viktoras Makauskas <viktoras@neglostyti.com>
2016-04-10 17:20:23 +0300

    trying to wrap up the POC and get it to work

--- cmd/gontractor_test.go
+++ cmd/gontractor_test.go
@@ -13,10 +13,10 @@ func requireFileExists(t *testing.T, file string) {
 
 func TestE2E(t *testing.T) {
 	g := NewGontractor()
-	g.spec = "../../test-resources/swagger.yaml"
-	g.apiOutFile = "../../test-output/testE2E/api/api.go_"
-	g.serverOutFile = "../../test-output/testE2E/server_generated.go_"
-	g.serverTemplate = "../../sample-templates/proprietary-api/server.tpl"
+	g.spec = "../test-resources/swagger.yaml"
+	g.apiOutFile = "../test-output/testE2E/api/api.go_"
+	g.serverOutFile = "../test-output/testE2E/server_generated.go_"
+	g.serverTemplate = "../sample-templates/proprietary-api/server.tpl"
 	g.Execute()
 
 	requireFileExists(t, g.apiOutFile)
@@ -46,8 +46,8 @@ func TestGetAbsolutePackagePath(t *testing.T) {
 		require.NoError(t, err)
 		return p
 	}
-	require.Equal(t, "github.com/viktorasm/gontractor/cmd/gontractor", noError(g.getAbsolutePackagePath("server.go")))
-	require.Equal(t, "github.com/viktorasm/gontractor/cmd/gontractor", noError(g.getAbsolutePackagePath("./server.go")))
-	require.Equal(t, "github.com/viktorasm/gontractor/cmd/gontractor/bar/foo", noError(g.getAbsolutePackagePath("bar/foo/server.go")))
-	require.Equal(t, "github.com/viktorasm/gontractor/cmd/gontractor/bar/foo", noError(g.getAbsolutePackagePath("./bar/foo/server.go")))
+	require.Equal(t, "github.com/viktorasm/gontractor/cmd", noError(g.getAbsolutePackagePath("server.go")))
+	require.Equal(t, "github.com/viktorasm/gontractor/cmd", noError(g.getAbsolutePackagePath("./server.go")))
+	require.Equal(t, "github.com/viktorasm/gontractor/cmd/bar/foo", noError(g.getAbsolutePackagePath("bar/foo/server.go")))
+	require.Equal(t, "github.com/viktorasm/gontractor/cmd/bar/foo", noError(g.getAbsolutePackagePath("./bar/foo/server.go")))
 }
